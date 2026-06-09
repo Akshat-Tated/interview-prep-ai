36fd5e69212627d20e505df4e046fae1c4223ecc
Akshat-Tated <akshattated18@gmail.com>
2026-06-09 09:46:37 +0530

    Create Session API

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const cors = require("cors")
 const path = require("path")
 const connectDB = require("./config/db")
 const authRoutes = require("./routes/auth.route")
+const sessionRoutes = require("./routes/session.route")
+
 
 
 const app = express()
@@ -23,6 +25,7 @@ app.use(express.json())
 
 // routes
 app.use("/api/auth", authRoutes)
+app.use("/api/sessions", sessionRoutes)
 
 // Server uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}))
